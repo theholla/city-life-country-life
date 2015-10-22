1705aa3d6bc4a787c5cd9acc407695bbd358ba0e
teresa fletcher & diana holland <student@epicodus.com>
2015-10-22 11:17:59 -0700

    Add image button and start to gallery

---
+++ app/src/main/java/com/epicodus/citylifecountrylife/CityActivity.java
@@ -0,0 +1,7 @@
+package com.epicodus.citylifecountrylife;
+
+/**
+ * Created by Guest on 10/22/15.
+ */
+public class CityActivity {
+}
--- app/src/main/java/com/epicodus/citylifecountrylife/MainActivity.java
+++ app/src/main/java/com/epicodus/citylifecountrylife/MainActivity.java
@@ -1,37 +1,52 @@
 package com.epicodus.citylifecountrylife;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton mMainCityButton, mMainCountryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Typeface neucha = Typeface.createFromAsset(getAssets(), "fonts/neucha.ttf");
+
+        TextView mainCityLabel = (TextView) findViewById(R.id.mainCityLabel);
+        mainCityLabel.setTypeface(neucha);
+
+        TextView mainCountryLabel = (TextView) findViewById(R.id.mainCountryLabel);
+        mainCountryLabel.setTypeface(neucha);
+
+        mMainCityButton = (ImageButton) findViewById(R.id.mainCityButton);
+        mMainCountryButton = (ImageButton) findViewById(R.id.mainCountryButton);
+
+        mMainCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CityActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mMainCountryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CountrysideActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
