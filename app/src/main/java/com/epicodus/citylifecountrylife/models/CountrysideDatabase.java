d19564c482be70dd5ccc5ceea4794053d3cb7841
diana holland & teresa fletcher <student@epicodus.com>
2015-10-22 16:36:43 -0700

    Add countryside gallery

--- app/src/main/java/com/epicodus/citylifecountrylife/models/CountrysideDatabase.java
+++ app/src/main/java/com/epicodus/citylifecountrylife/models/CountrysideDatabase.java
@@ -1,7 +1,62 @@
 package com.epicodus.citylifecountrylife.models;
 
+import com.epicodus.citylifecountrylife.R;
+
+import java.util.ArrayList;
+
 /**
  * Created by Teresa and Diana on 10/22/15.
  */
 public class CountrysideDatabase {
-}
+        private ArrayList<Countryside> mCountrysides;
+
+        public CountrysideDatabase() {
+            setCountryside();
+        }
+    private void setCountryside() {
+        mCountrysides = new ArrayList<>();
+
+        mCountrysides.add(new Countryside(
+                "Rural North, Florida",
+                R.drawable.country_florida,
+                R.drawable.country_floridapanhandle
+        ));
+
+        mCountrysides.add(new Countryside(
+                "Semuc Champey, Guatemala",
+                R.drawable.country_guatemala,
+                R.drawable.country_guatemala
+        ));
+
+        mCountrysides.add(new Countryside(
+                "Rural, Ireland",
+                R.drawable.country_ireland,
+                R.drawable.country_ireland
+        ));
+    }
+
+    public ArrayList<Countryside> getCountrysides() {
+        return mCountrysides;
+    }
+
+    public Countryside nextCountryside(Countryside currentCountryside) {
+        int index = mCountrysides.indexOf(currentCountryside);
+        if (index == mCountrysides.size() - 1) {
+            return mCountrysides.get(0);
+        } else {
+            return mCountrysides.get(index + 1);
+        }
+    }
+
+    public Countryside lastCountryside(Countryside currentCountryside) {
+        int index = mCountrysides.indexOf(currentCountryside);
+        if (index == mCountrysides.size() - 1) {
+            return mCountrysides.get(0);
+        } else if (index == 0){
+            return currentCountryside;
+        } else{
+            return mCountrysides.get(index - 1);
+        }
+    }
+
+    }
--- app/src/main/java/com/epicodus/citylifecountrylife/ui/CountrysideActivity.java
+++ app/src/main/java/com/epicodus/citylifecountrylife/ui/CountrysideActivity.java
@@ -4,16 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.epicodus.citylifecountrylife.R;
+import com.epicodus.citylifecountrylife.models.City;
+import com.epicodus.citylifecountrylife.models.CityDatabase;
+import com.epicodus.citylifecountrylife.models.Countryside;
+import com.epicodus.citylifecountrylife.models.CountrysideDatabase;
 
 public class CountrysideActivity extends AppCompatActivity {
 
     private TextView mCountryside;
     private ImageView mImage;
     private ImageView mImage2;
+    private ImageButton mNextButton;
+    private ImageButton mCountrysideNextButton;
+
+    private CountrysideDatabase mCountrysideDatabase;
+    private Countryside mCurrentCountryside;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,36 @@ public class CountrysideActivity extends AppCompatActivity {
         mCountryside = (TextView) findViewById(R.id.countryTextView);
         mImage = (ImageView) findViewById(R.id.countryImageView);
         mImage2 = (ImageView) findViewById(R.id.countrySecondImageView);
+        mCountrysideDatabase = new CountrysideDatabase();
+        mCurrentCountryside = mCountrysideDatabase.getCountrysides().get(0);
+        mNextButton = (ImageButton) findViewById(R.id.nextButton);
+        mCountrysideNextButton = (ImageButton) findViewById(R.id.countrysideNextButton);
+        setLayoutContent();
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentCountryside = mCountrysideDatabase.nextCountryside(mCurrentCountryside);
+                setLayoutContent();
+
+            }
+        });
+
+        mCountrysideNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentCountryside = mCountrysideDatabase.lastCountryside(mCurrentCountryside);
+                setLayoutContent();
+            }
+        });
+
     }
 
-}
+
+    private void setLayoutContent() {
+        mCountryside.setText(mCurrentCountryside.getCountryside());
+        mImage.setImageResource(mCurrentCountryside.getImage());
+        mImage2.setImageResource(mCurrentCountryside.getImage2());
+    }
+
+};
